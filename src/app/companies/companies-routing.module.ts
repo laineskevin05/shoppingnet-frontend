9eca927b3f6d12584aed24a57274fcfc977dc3bc
Kevin Darel Laines <lainezkevin05@gmail.com>
2022-07-17 00:07:58 -0600

    Empresa lista de paginas

---
+++ src/app/companies/companies-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MainComponent } from './pages/main/main.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { PaginasComponent } from './pages/paginas/paginas.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: MainComponent,
+    children: [
+      { path: '', component: DashboardComponent },
+      { path: 'pages', component: PaginasComponent },
+      { path: '**', redirectTo: '' },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class CompaniesRoutingModule {}
