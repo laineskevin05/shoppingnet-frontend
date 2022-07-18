1f39936af9a2e13d1b0a9db13c803332411468e2
melvinmaldonado22 <xmelvinymaldonadozmoncada@gmail.com>
2022-07-17 22:49:45 -0600

    Navegación
    Navegacion entre componetes de registro

--- src/app/auth/pages/registro-empresa/registro-empresa.component.ts
+++ src/app/auth/pages/registro-empresa/registro-empresa.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-registro-empresa',
   templateUrl: './registro-empresa.component.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroEmpresaComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  registrar(){
+    this.router.navigateByUrl('/auth/registro-usuario');
+  }
 }
--- src/app/auth/pages/registro-usuario/registro-usuario.component.ts
+++ src/app/auth/pages/registro-usuario/registro-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-registro-usuario',
   templateUrl: './registro-usuario.component.html',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroUsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+    
+  }
 
   ngOnInit(): void {
+  }
+
+
+  registrar(){
+    this.router.navigateByUrl('/auth/registro-empresa');
   }
 
 }
