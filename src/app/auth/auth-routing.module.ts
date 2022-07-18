c965149562e58b69f90a7ca2cf70f47444c61673
melvinmaldonado22 <xmelvinymaldonadozmoncada@gmail.com>
2022-07-17 22:08:33 -0600

    Registro-Usiario
    Estilos

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -4,12 +4,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { RegistroEmpresaComponent } from './pages/registro-empresa/registro-empresa.component';
+import { RegistroUsuarioComponent } from './pages/registro-usuario/registro-usuario.component';
 
 const routes: Routes = [
   {
     path: '',
     component: MainComponent,
     children: [
+      { path: 'registro-empresa', component:RegistroEmpresaComponent },
+      { path: 'registro-usuario', component:RegistroUsuarioComponent },
       { path: 'login', component: LoginComponent },
       { path: 'registro', component: RegisterComponent },
       { path: '**', redirectTo: 'login' },
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -6,10 +6,12 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
 import { RegisterComponent } from './pages/register/register.component';
 import { MainComponent } from './pages/main/main.component';
+import { RegistroEmpresaComponent } from './pages/registro-empresa/registro-empresa.component';
+import { RegistroUsuarioComponent } from './pages/registro-usuario/registro-usuario.component';
 
 
 @NgModule({
-  declarations: [LoginComponent, RegisterComponent, MainComponent],
+  declarations: [LoginComponent, RegisterComponent, MainComponent, RegistroEmpresaComponent, RegistroUsuarioComponent],
   imports: [
     CommonModule,
     AuthRoutingModule,
